c6fd3fdc76ef5fa8025a788df40e932b75ea9368
Mayckonjose <145240224+Mayckonjose@users.noreply.github.com>
2024-11-25 19:26:13 -0300

    first commit

---
+++ steam_clone_main/users/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Conta criada para {username}! Você agora pode fazer login.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+def login_view(request):
+    return render(request, 'users/login.html')
